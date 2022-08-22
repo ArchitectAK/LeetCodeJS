01b32a9282a64b37d2c68ea0bb06e0242400f942
ArchitectAK <ankitdroiddeveloper@gmail.com>
2022-08-22 06:47:45 +0530

    Add solution for 342. Power of Four

---
+++ Solutions/342. Power of Four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfFour = function(n) {
+  if(n == 0) return false;
+  
+  if(n == 1) return true;
+  
+  if(n % 4 != 0) return false;  
+  
+  return isPowerOfFour(n/4);
+};
