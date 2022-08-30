2f073daabe90b3f87edbb1f519ae4ef7e5956a72
ArchitectAK <ankitdroiddeveloper@gmail.com>
2022-08-30 15:17:29 +0530

    Refactor 48. Rotate Image

--- Solutions/48. Rotate Image.js
+++ Solutions/48. Rotate Image.js
@@ -3,20 +3,22 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
  var rotate = function(matrix) {
-  for (let i=0;i<matrix.length;i++) {
-       for (let j=i;j<matrix[0].length;j++) {
-           let temp = matrix[i][j];
-           matrix[i][j] = matrix[j][i];
-           matrix[j][i] = temp;
-       }
-   }
+  let m = matrix[0].length;
+  let n = matrix.length;
+  for (let i=0;i < n; i++) {
+      for (let j=i;j < m;j++) {
+          let temp = matrix[i][j];
+          matrix[i][j] = matrix[j][i];
+          matrix[j][i] = temp;
+      }
+  }
 
-   for (let i=0;i<matrix.length;i++) {
-       for (let j=0;j<matrix[0].length/2;j++) {
-           let temp = matrix[i][j];
-           matrix[i][j] = matrix[i][matrix[0].length-j-1];
-           matrix[i][matrix[0].length-j-1] = temp;
-       }
-   }
-   return matrix;
+  for (let i = 0;i < n; i++) {
+      for (let j = 0;j <m/2; j++) {
+          let temp = matrix[i][j];
+          matrix[i][j] = matrix[i][m - j - 1];
+          matrix[i][m - j - 1] = temp;
+      }
+  }
+  return matrix;
 };
